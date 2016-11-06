d2eda2c65b18a76e099f7b66f70f9ab859adaed9
voyagerok <shepard1010@gmail.com>
2016-11-06 17:08:56 +0300

    initial commit

---
+++ mediaplayer/src/main.cpp
@@ -0,0 +1,18 @@
+/*
+ ============================================================================
+ Name        : mediaplayer.cpp
+ Author      : nikolay
+ Version     :
+ Copyright   : This file is part of Mediaplayer.  Mediaplayer is free software: you can redistribute it and/or modify it under the terms of the GNU General Public License as published by the Free Software Foundation, either version 3 of the License, or (at your option) any later version. Mediaplayer is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License for more details. You should have received a copy of the GNU General Public License along with Mediaplayer.  If not, see <http://www.gnu.org/licenses/>.
+ Description : Hello World in C++,
+ ============================================================================
+ */
+
+#include <iostream>
+
+using namespace std;
+
+int main(void) {
+	cout << "Hey" << endl; /* prints Hey */
+	return 0;
+}
